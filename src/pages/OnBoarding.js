2b6ca3cf3ec08aff2352482c03e5bbad4a121b84
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-03-12 14:44:15 +0100

    Actualizando Usuarios

--- src/pages/OnBoarding.js
+++ src/pages/OnBoarding.js
@@ -1,26 +1,40 @@
 import {useState} from "react"
 import Nav from '../components/Nav'
+import { useCookies} from "react-cookie"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 
 const OnBoarding = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
     const [formData, setFormData] = useState({
-        user_id: '',
+        user_id: cookies.UserId,
         first_name: '',
         dob_day: '',
         dob_month: '',
         dob_year: '',
         show_gender: false,
-        gender_identity: '',
-        gender_interest: '',
+        gender_identity: 'man',
+        gender_interest: 'woman',
         email: '',
         url: '',
         about: '',
         matches: []
     })
 
+    let navigate = useNavigate()
 
-    const handleSubmit = () => {
-        console.log("submitted")
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault()
+        try {
+            const response = await axios.put('http://localhost:8000/user', { formData })
+            const success = response.status === 200
+            if (success) navigate('/dashboard')
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     const handleChange = (e) => {
@@ -184,7 +198,7 @@ const OnBoarding = () => {
                             required={true}
                         />
                         <div className="photo-container">
-                            <img src={formData.url} alt="profile pic preview"/>
+                            {formData.url && <img src={formData.url} alt="profile pic preview"/>}
                         </div>
                     </section>
 
